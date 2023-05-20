94cdb37d15840db0428d377803da085df0fdedf3
Tifoysal <foysal11203004@gmail.com>
2023-05-20 14:44:08 +0600

    add apsos

--- app/Http/Controllers/UniversitiesController.php
+++ app/Http/Controllers/UniversitiesController.php
@@ -7,5 +7,8 @@ use Tifoysal\BangladeshUniversities\Models\Universities;
 
 class UniversitiesController extends Controller
 {
-
+public function list()
+{
+    echo "apsos mitu cumilla jay nai ";
+}
 }
